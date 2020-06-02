655958604d6350530bb592a8d55330a6aba60827
charlie-grayson <57486129+charlie-grayson@users.noreply.github.com>
2020-06-02 10:23:09 +0100

    Update IssueICACertCodehook.js

--- IssueICACertCodehook.js
+++ IssueICACertCodehook.js
@@ -4,7 +4,7 @@ function beforePOST(req, res){
 //  log.debug("REQ.HINT: ",req.hint);
   var slackhookurl = <<replace with slackhook url for your slack channel>>;
   var issuerDnO = req.body['issuerDnO'];
-  
+  // Message Output
   var slackmatch = {
     "message": `WBA PMO created a new WBA PKI Issuing I-CA certificate issued by ${req.body['issuerDnO']} \nSubject \nO=${req.body['subjectDnO']} \nOU=${req.body['subjectDnOu']} \nC=${req.body['subjectDnC']}`,
     "slackhookurl": slackhookurl,
