cf567cae58b2448582fc042210652ca3d1ddae0b
charlie-grayson <57486129+charlie-grayson@users.noreply.github.com>
2020-09-14 18:01:53 +0100

    Update wba_subid_upload.py

--- python/wba_subid_upload.py
+++ python/wba_subid_upload.py
@@ -3,95 +3,75 @@
 # WBA Bulk upload script for Subordinate IDs
 # takes input from XLSX and uploads to restdb.io
 
-import pandas as pd
-import os.path
-import sys
-import requests
-import re
+def set_api_and_url(filename):
+    config = open(filename, "r")
+    # config.txt has four lines formatted as follows
+    # MEMBER-URL:<URL of member database>
+    # SUBID-URL:<URL of end-entity database>
+    # CERT-URL:<URL of certificate database>
+    # API-KEY:<Master API key for database>
 
-pd.set_option('display.max_rows', None)
-dirpath = os.path.dirname(sys.argv[0])
+    # config.txt file must be in same directoy as python .py
 
-if (sys.platform == "win32"):
-    config_file = filename = os.path.join(dirpath + "/", "config.cfg")
-else:
-    config_file = filename = os.path.join(dirpath, "config.cfg")
+    member_url = config.readline() #dummy read not used in this script
+    local_url = config.readline()
+    cert_url = config.readline() #dummy read not used in this script
+    local_apikey = config.readline()
 
-print ("WBA BULK INSTALL SUBORDINATE ID LIST FROM XLS TO RESTDB")
-if os.path.exists(config_file):
+    local_url = local_url[local_url.find(":") + 1:-1]
+    local_apikey = local_apikey[local_apikey.find(":") + 1:-1]
 
-    config = open(config_file,"r")
-#config.txt has four lines formatted as follows
-#MEMBER-URL:<URL of member database>
-#SUBID-URL:<URL of end-entity database>
-#CERT-URL:<URL of certificate database>
-#API-KEY:<Master API key for database>
+    return (local_url,local_apikey)
 
-#config.txt file must be in same directoy as python .py
+def enter_and_check_excel(path):
+    import pandas as pd
+    import os.path
 
-    member_url = config.readline()
-    url = config.readline()
-    cert_url = config.readline()
-    apikey = config.readline()
-
-    url = url[url.find(":")+1:-1]
-    apikey = apikey[apikey.find(":")+1:-1]
-
+    pd.set_option('display.max_rows', None)
     check = False
     cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    resources = ["CompanyName", "CompanyAddress", "CompanyPhone", "ContactName", "ContactEmail", "ContactPhone", "WBAMember", "WBAAgent", "WBAID", "EntityType"]
-#these column headings are checked to see if they are correct in the xlsx
+    resources = ["CompanyName", "CompanyAddress", "CompanyPhone", "ContactName", "ContactEmail", "ContactPhone",
+                 "WBAMember", "WBAAgent", "WBAID", "EntityType"]
 
-
-    while check==False:
+    while check == False:
         check = True
         name = input("Enter filename of xls to upload: ")
-        
-        if (sys.platform == "win32"):
-            filename = os.path.join(dirpath + "/", name)
-        else:
-            filename = os.path.join(dirpath, name)
+        filename = os.path.join(path, name)
 
         try:
             pd.read_excel(filename, na_values=["", " ", "-"])
         except NameError:
             check = False
-            print ("File Not Found.")
+            print("File Not Found.")
         except Exception:
             check = False
-            print ("Error in reading", filename)
+            print("Error in reading", filename)
         if check == True:
             try:
-                data = pd.read_excel(filename, sheet_name='SubIDs', usecols=cols, skiprows = 3)
-            except Exception:
+                local_data = pd.read_excel(filename, sheet_name='SubIDs', usecols=cols, skiprows=3)
+            except Exception as Ex:
                 check = False
                 print("Sheet named SubIDs not found")
         if check == False:
             print(' Please re-enter')
 
-    # check columns are formtted correctly
-    i=0
-    for col in data.columns:
-        if (col!=resources[i]):
-            check = False
-            print('Sheet named SubIDs  has badly formated columns')
+    #  check columns are formtted correctly
+    i = 0
+    for col in local_data.columns:
+        if (col != resources[i]):
+            print('Sheet named SubIDs has badly formatted columns')
             exit()
-        i=i+1
+        i = i + 1
+    check_regex(local_data)
+    return (local_data)
 
-
-    df = pd.read_excel (filename, sheet_name='SubIDs', usecols=cols, skiprows = 3)
-
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-    headers["x-apikey"] = apikey
+def check_regex(local_data):
+    import re
 
     # REGEXP Checking
     regexp = [".*", ".*", ".*", ".*", ".*", ".*", "^((Yes)|(No))$", "^[^_]*$", "^[^_]*$", "^((VNP)|(HSP)|(VNP&HSP))$"]
 
-
-    for index, row in df.iterrows():
+    for index, row in local_data.iterrows():
         a = re.match (regexp[6], row['WBAMember'])
         b = re.match (regexp[7], row['WBAAgent'])
         c = re.match(regexp[8], row['WBAID'])
@@ -102,27 +82,51 @@ if os.path.exists(config_file):
             print("SubID Record ", index, "REGEXP check FAILED - please correct and try again")
             exit()
 
-    #code to check GET CURL works
-#    response = requests.get(url, headers=headers)
-#    print (response, response.text, response.json())
+    return
 
+def post_data(local_data, local_url, local_apikey):
+    import requests
 
-    #Check that CURL POST is working
-#    json = "{ \"Company\": \"Dummy Company\" , \"Category\": \"General\", \"Contact\": \"John Smith\", \"PrimaryID\": \"Dummy\", \"CountryCode\": \"US\" }"
-#    response = requests.post(url, headers=headers, data=json)
-#    print (response.text, response.json)
-#    print ("status code ", response.status_code)
+    headers = {
+        "Content-Type": "application/json"
+    }
+    headers["x-apikey"] = local_apikey
 
-    print ("Parsed Array Information")
-    for index, row in df.iterrows():
-        json = "{ \"CompanyName\": \"" + row['CompanyName'] + "\" , \"CompanyAddress\": \"" + row['CompanyAddress'] + "\" , \"CompanyPhone\": \"" + row['CompanyPhone'] + "\", \"ContactName\": \"" + row['ContactName'] + "\", \"ContactEmail\": \"" + row ['ContactEmail'] + "\" , \"ContactPhone\": \"" + row ['ContactPhone'] + "\" , \"WBAMember\": \"" + row ['WBAMember'] + "\" , \"WBAAgent\": \"" + row ['WBAAgent'] + "\"  , \"WBAID\": \"" + row ['WBAID'] + "\"  , \"EntityType\": \"" + row ['EntityType'] + "\" }"
+    print("Parsed Array Information")
+    for index, row in local_data.iterrows():
+        json = "{ \"CompanyName\": \"" + row['CompanyName'] + "\" , \"CompanyAddress\": \"" + row[
+            'CompanyAddress'] + "\" , \"CompanyPhone\": \"" + row['CompanyPhone'] + "\", \"ContactName\": \"" + row[
+                   'ContactName'] + "\", \"ContactEmail\": \"" + row['ContactEmail'] + "\" , \"ContactPhone\": \"" + \
+               row['ContactPhone'] + "\" , \"WBAMember\": \"" + row['WBAMember'] + "\" , \"WBAAgent\": \"" + row[
+                   'WBAAgent'] + "\"  , \"WBAID\": \"" + row['WBAID'] + "\"  , \"EntityType\": \"" + row[
+                   'EntityType'] + "\" }"
         # check that new database has same field names as above
-        print (json)
-        response = requests.post(url, headers=headers, data=json)
-        if response.status_code>=400:
-            print ("Error uploading new SubID ", row['WBAID'], " to the database")
-            print (response.text)
-else:
-    print("config.cfg file Not Found.")
+        print(json)
+        response = requests.post(local_url, headers=headers, data=json)
+        if response.status_code >= 400:
+            print("Error uploading new SubID ", row['WBAID'], " to the database")
+            print(response.text)
+    return
 
 
+def main():
+    import os.path
+    import sys
+
+    print ("WBA BULK INSTALL SUBORDINATE ID LIST FROM XLS TO RESTDB")
+
+    dirpath = os.path.dirname(sys.argv[0])
+    config_file = os.path.join(dirpath, "config.cfg")
+
+
+    if os.path.exists(config_file):
+
+        url, apikey = set_api_and_url(config_file)
+        df = enter_and_check_excel(dirpath)
+        post_data(df,url,apikey)
+
+    else:
+        print("config.cfg file Not Found.")
+
+if __name__ == "__main__":
+    main()
